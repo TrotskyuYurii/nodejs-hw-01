7478e61f8ef8c6f3c3ffcd9e1faa13eac5b8893a
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-05-18 16:59:19 +0300

    step_4

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+
+    try {
+        const contacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+        return contacts;
+    } catch (error) {
+        console.log(error);
+    }
+
+};
 
 console.log(await getAllContacts());
