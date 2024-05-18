b6faa4d3a7b41d0d7c80f3627021e9f68e44fa60
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-05-18 17:19:09 +0300

    RC1

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const allContacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+    const survivorsContacts = allContacts.filter(contact => {
+      const variation = Math.random();
+      return variation > 0.5; 
+    });
+    // console.log(survivorsContacts);
+    await fs.writeFile(PATH_DB, JSON.stringify(survivorsContacts));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 await thanos();
