43a361c656fcfeb3a89ec7291ff941704db93ec1
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-05-18 17:00:53 +0300

    step_5

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+
+    try {
+        const contacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+        return contacts.length;
+    } catch (error) {
+        console.log(error);
+    }
+
+};
 
 console.log(await countContacts());
